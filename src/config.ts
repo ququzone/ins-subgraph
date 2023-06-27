d13d0538f22096902f4ec6150954ebe6f7f249ac
ququzone <xueping.yang@gmail.com>
2023-06-27 16:01:34 +0800

    fix bigint bug

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ let NAME_WRAPPER_ADDRESS_TEMP: string;
 
 if (dataSource.network() == Network.IOTEX.toLowerCase()) {
   NAME_WRAPPER_ADDRESS_TEMP = "0x26DEFAD6414321b13028A072a46D88400e94abb3";
-} else if (dataSource.network() == Network.IOTEX.toLowerCase()) {
+} else if (dataSource.network() == Network.IOTEX_TESTNET.toLowerCase()) {
   NAME_WRAPPER_ADDRESS_TEMP = "0x92f0926350268a0147E36c4Dfbc4c72Eb11696cF";
 }
 
